544e2dcc147f0b5358a273a0fab75fdd3e1eb22d
YingYinJed <1105752660@qq.com>
2021-06-20 17:28:50 +0800

    初始化

---
+++ OV2640/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'OV2640' 
+ * Target:  'OV2640' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
